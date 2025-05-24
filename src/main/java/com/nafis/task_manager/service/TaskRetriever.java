23221e08125f147c727ece401764742a9600d9d3
nafis-shahriar <nstshahriar01@gmail.com>
2025-05-24 16:43:18 +0600

    feat: add custom task not found exception

--- src/main/java/com/nafis/task_manager/service/TaskRetriever.java
+++ src/main/java/com/nafis/task_manager/service/TaskRetriever.java
@@ -4,6 +4,7 @@ package com.nafis.task_manager.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nafis.task_manager.dto.Task;
+import com.nafis.task_manager.exception.TaskNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.InternalException;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,7 +23,7 @@ public class TaskRetriever {
     @Value("${task.folder.path:/home/nafis/Documents}")
     private String taskFolderPath;
 
-    public Optional<Task> getById(String id) {
+    public Task getById(String id) {
 
         final var folder = "/Tasks";
 
@@ -30,19 +31,26 @@ public class TaskRetriever {
         File file = filePath.toFile();
 
         if (!file.exists() || !file.isFile()) {
-            return Optional.empty();
+
+            log.error("Task with id {} not found", id);
+
+            throw new TaskNotFoundException("Task not found with ID: " + id);
         }
 
         try {
-            Task task = objectMapper.readValue(file, Task.class);
-            return Optional.ofNullable(task);
+
+            return objectMapper.readValue(file, Task.class);
 
         } catch (JsonProcessingException e) {
 
-            log.error("Failed to parse task", e);
+            log.error("Failed to parse json", e);
+
             throw new RuntimeException("Malformed json", e);
 
         } catch (Exception e) {
+
+            log.error("Failed to retrieve task", e);
+
             throw new InternalException("Failed to retrieve task", e);
         }
     }
