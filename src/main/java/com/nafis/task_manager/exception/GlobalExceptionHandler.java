679b2678e4decb3a8ac2a2d845bb450583a894db
nafis-shahriar <nstshahriar01@gmail.com>
2025-05-24 16:43:53 +0600

    feat:add global exception handler

---
+++ src/main/java/com/nafis/task_manager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.nafis.task_manager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Task not found.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong");
+    }
+}
